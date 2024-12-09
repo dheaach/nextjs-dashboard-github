2297e40dd9fdbc7d55d074f8bb7bc3ebea0741c2
Dhea Sukma <dheaach10@gmail.com>
2024-12-09 16:27:28 +0700

    customers update

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -23,6 +23,8 @@ const FormSchema = z.object({
  
 const CreateInvoice = FormSchema.omit({ id: true, date: true });
 const UpdateInvoice = FormSchema.omit({ id: true, date: true });
+const CreateCustomer = FormSchema.omit({ id: true, date: true });
+const UpdateCustomer = FormSchema.omit({ id: true, date: true });
 
 export type State = {
   errors?: {
@@ -136,4 +138,55 @@ export async function authenticate(
     }
     throw error;
   }
+}
+
+export async function deleteCustomer(id: string) {
+  throw new Error('Failed to Delete Invoice');
+  
+  try {
+      await sql`DELETE FROM customers WHERE id = ${id}`;
+      revalidatePath('/dashboard/customers');
+      return { message: 'Deleted Customers.' };
+  } catch (error) {
+      return { message: 'Database Error: Failed to Delete Customers.' };
+  }
+}
+
+export async function createCustomer(prevState: State, formData: FormData) {
+  // Validate form using Zod
+  const validatedFields = CreateCustomer.safeParse({
+    customerId: formData.get('customerId'),
+    amount: formData.get('amount'),
+    status: formData.get('status'),
+  });
+ 
+  // If form validation fails, return errors early. Otherwise, continue.
+  if (!validatedFields.success) {
+    return {
+      errors: validatedFields.error.flatten().fieldErrors,
+      message: 'Missing Fields. Failed to Create Invoice.',
+    };
+  }
+ 
+  // Prepare data for insertion into the database
+  const { customerId, amount, status } = validatedFields.data;
+  const amountInCents = amount * 100;
+  const date = new Date().toISOString().split('T')[0];
+ 
+  // Insert data into the database
+  try {
+    await sql`
+      INSERT INTO invoices (customer_id, amount, status, date)
+      VALUES (${customerId}, ${amountInCents}, ${status}, ${date})
+    `;
+  } catch (error) {
+    // If a database error occurs, return a more specific error.
+    return {
+      message: 'Database Error: Failed to Create Invoice.',
+    };
+  }
+ 
+  // Revalidate the cache for the invoices page and redirect the user.
+  revalidatePath('/dashboard/invoices');
+  redirect('/dashboard/invoices');
 }
